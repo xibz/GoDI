c573ceb72806f430836b50453b535b462481fb55
Ben <impactbchang@gmail.com>
2014-05-31 04:46:43 -0700

    Added factories

--- Injector/injector.go
+++ Injector/injector.go
@@ -6,11 +6,13 @@ import (
 
 type Container struct {
   injMap map[string] interface{}
+  factoryMethod map[string] (func() interface{})
 }
 
 func New() *Container {
   c := Container{}
   c.injMap = make(map[string] interface{})
+  c.factoryMethod = make(map[string] (func() interface{}))
   return &c
 }
 func (c *Container) Register(obj interface{}) {
@@ -21,4 +23,12 @@ func (c *Container) Register(obj interface{}) {
 func (c *Container) BindToInterface(s interface{}, objName string) interface{} {
   s = c.injMap[objName]
   return s
+}
+
+func (c *Container) BindFactoryMethod(name string, f func() interface{}) {
+  c.factoryMethod[name] = f
+}
+
+func (c *Container) CallFactoryMethod(name string) interface{} {
+  return c.factoryMethod[name]()
 }
--- UnitTests/Injector/injector_test.go
+++ UnitTests/Injector/injector_test.go
@@ -3,7 +3,6 @@ package UnitTest
 import (
   "testing"
   "GoDI/Injector"
-  //"reflect"
 )
 
 type Animal interface {
@@ -26,15 +25,28 @@ func (c Cat) Speak() string {
   return c.Sound
 }
 
+func BuildDog() interface{} {
+  return &Dog{"WOOF"}
+}
+
 func Test_Injector(t *testing.T) {
   cnt := GoDI.New()
   d := Dog{"WOOF"}
   c := Cat{"MEOW"}
   cnt.Register(d)
   cnt.Register(c)
-  s := cnt.BindToInterface(new(Animal), "Dog").(Animal)
-  r := cnt.BindToInterface(new(Animal), "Cat").(Animal)
-  if s.Speak() != "WOOF" && r.Speak() != "MEOW" {
+  s := cnt.BindToInterface(new(Animal), "Dog")
+  r := cnt.BindToInterface(new(Animal), "Cat")
+  if s.(Animal).Speak() != "WOOF" && r.(Animal).Speak() != "MEOW" {
+    t.Errorf("Injector did not produce right result")
+  }
+}
+
+func Test_Factory(t *testing.T) {
+  cnt := GoDI.New()
+  cnt.BindFactoryMethod("Dog", BuildDog)
+  d := cnt.CallFactoryMethod("Dog").(Animal)
+  if d.Speak() != "WOOF" {
     t.Errorf("Injector did not produce right result")
   }
 }
